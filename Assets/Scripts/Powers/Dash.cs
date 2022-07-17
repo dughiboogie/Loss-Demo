7da3969e6bea2137cbc793994785313aa8160ce5
Marco Dugatto <dughi.boogie@gmail.com>
2022-07-17 22:37:03 +0200

    [REFACTORING] Individual powers
    Added individual powers to remove their logic from PlayerController

---
+++ Assets/Scripts/Powers/Dash.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Dash : MonoBehaviour
+{
+    private PlayerController2D instance;
+
+    [SerializeField] private float dashValue = 20f;
+    [SerializeField] private float dashDuration = .15f;
+    [SerializeField] private float dashCooldown = 1f;
+    private int maxDashInAir = 1;
+    private int dashInAirCounter = 0;
+
+    private float currentDashDuration = 0f;
+    public float CurrentDashDuration
+    {
+        get { return currentDashDuration; }
+        set { currentDashDuration = value; }
+    }
+
+    private float dashCooldownCounter = 0f;
+    public float DashCooldownCounter
+    {
+        get { return dashCooldownCounter; }
+        set { dashCooldownCounter = value; }
+    }
+
+    private void Awake()
+    {
+        instance = PlayerController2D.instance;
+    }
+
+    private void Update()
+    {
+        // When player hits the ground or sticks to a wall reset the air dash counter
+        if(isGrounded || isWallSliding || isHanging) {
+            dashInAirCounter = 0;
+            if(!wasOnGround) {
+                dashCooldownCounter = 0;
+            }
+        }
+
+        // Subtract from dash cooldown every frame if cooldown is > 0
+        if(dashCooldownCounter > 0) {
+            dashCooldownCounter -= Time.deltaTime;
+        }
+
+        // If dash duration is > 0 it means that the player is dashing
+        if(currentDashDuration > 0) {
+            // If player is onto a wall dash in the opposite direction
+            if(isWallSliding) {
+                // Count the dash from the wall as a dash in mid-air
+                dashInAirCounter++;
+                FlipPlayerObject();
+            }
+
+            // Prevent new inputs if player is dashing
+            InputManager.instance.BlockInput(currentDashDuration);
+
+            SetRigidbodyVelocity(dashValue * facingDirection, 0f);
+
+            currentDashDuration -= Time.deltaTime;
+        }
+
+    }
+
+    #region Input actions
+
+    /*
+     * Thrust player forward and set a cooldown for the next time he can dash.
+     * If player is in mid-air he can dash only one time.
+     */
+    public void TryDash(InputAction.CallbackContext context)
+    {
+        if(PowersManager.instance.IsPowerActive("Dash") && context.performed) {
+            if(!isHanging && dashCooldownCounter <= 0) {
+                if(isGrounded) {
+                    dashCooldownCounter = dashCooldown;
+                    currentDashDuration = dashDuration;
+                    GFX.Dash();
+                }
+
+                if(!isGrounded && dashInAirCounter < maxDashInAir) {
+                    dashCooldownCounter = dashCooldown;
+                    currentDashDuration = dashDuration;
+                    dashInAirCounter++;
+                    GFX.Dash();
+                }
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Powers/LedgeGrab.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LedgeGrab : MonoBehaviour
+{
+    private PlayerStatusManager playerStatusManager;
+
+    [SerializeField] private Transform ledgeGrab;
+
+    private float ledgeHangCooldown = .1f;
+    private float ledgeHangCooldownCounter;
+
+    private void Awake()
+    {
+        playerStatusManager = PlayerStatusManager.instance;
+    }
+
+    private void Update()
+    {
+        if(PowersManager.instance.IsPowerActive("Ledge Grab")) {
+
+            // Reset ledge grab cooldown (prevent grabbing the same ledge over and over)
+            if(ledgeHangCooldownCounter > 0) {
+                ledgeHangCooldownCounter -= Time.deltaTime;
+            }
+            // Grab ledge in mid air
+            else if(PlayerStatusManager.instance.IsNearLedgeMidAir) {
+
+                // TODO move to GFX
+               
+                transform.position = new Vector2(transform.position.x - (PlayerStatusManager.instance.FacingDirection * .25f), transform.position.y);
+
+                PlayerStatusManager.instance.IsGrabbingLedge = true;
+                NewPlayerController.instance.SetRigidbodyVelocity(Vector2.down * 7f); // Accelerate rigidbody to make grab more responsive
+            }
+        }
+    }
+}
